19a84c7be59959add11cbb970889ea833afdaad4
Rushil10 <rushil.10.shah@gmail.com>
2021-11-14 13:36:22 +0530

    changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,4 @@
 //export const url = 'https://api.directmart.xyz'
-export const url = 'http://13.233.82.172:3000'
+//export const url = 'http://13.233.82.172:3000'
 //export const url = 'http://98e89b9b37a8.ngrok.io'
+export const url = 'https://finalstobac.herokuapp.com'
